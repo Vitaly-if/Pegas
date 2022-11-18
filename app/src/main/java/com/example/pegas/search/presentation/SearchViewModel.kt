6a252ba0e0e36410b7d648ae1694ad7c33aacfe3
Adminius <vnakvasin@yandex.ru>
2022-11-18 16:17:55 +0700

    fix SearchViewModel.kt

--- app/src/main/java/com/example/pegas/search/presentation/SearchViewModel.kt
+++ app/src/main/java/com/example/pegas/search/presentation/SearchViewModel.kt
@@ -1,17 +1,46 @@
 package com.example.pegas.search.presentation
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.example.pegas.main.presentation.NavigationCommunication
 import com.example.pegas.main.presentation.NavigationStrategy
 import com.example.pegas.main.presentation.Screen
+import com.example.pegas.search.domain.SearchIteractor
 
-interface SearchViewModel {
-    fun showResponse()
+interface SearchViewModel : ClearError, ObserveSearch, UpdateState {
+    fun showResponse(toString: String)
     class Base(
-        private val navigationCommunication: NavigationCommunication.Mutate
+        private val communication: SearchCommunication,
+        private val navigationCommunication: NavigationCommunication.Mutate,
+        private val iteractor: SearchIteractor,
     ) : ViewModel(), SearchViewModel {
-        override fun showResponse() {
+        override fun showResponse(inputId: String) {
+            iteractor.saveIdForwarDoc(inputId)
             navigationCommunication.map(NavigationStrategy.Add(Screen.Responce))
+            communication.showState(UiState.Gone())
+        }
+
+        override fun clearError() {
+            communication.showState(UiState.CleanError())
+        }
+
+        override fun observerProgress(owner: LifecycleOwner, observer: Observer<Int>) {
+
+        }
+
+        override fun observerState(owner: LifecycleOwner, observer: Observer<UiState>) =
+            communication.observerState(owner, observer)
+
+        override fun updateState() {
+            communication.showState(UiState.Success())
         }
     }
+}
+
+interface ClearError {
+    fun clearError()
+}
+interface UpdateState {
+    fun updateState()
 }
