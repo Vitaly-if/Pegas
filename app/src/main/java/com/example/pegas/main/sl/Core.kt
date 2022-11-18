10e811a5efcda0b0e7977f4fe10345a621286651
Adminius <vnakvasin@yandex.ru>
2022-11-18 16:15:27 +0700

    fix Communication.kt Core.kt DependencyContainer.kt

--- app/src/main/java/com/example/pegas/main/sl/Core.kt
+++ app/src/main/java/com/example/pegas/main/sl/Core.kt
@@ -3,17 +3,20 @@ package com.example.pegas.main.sl
 import android.content.Context
 import com.example.pegas.main.presentation.DispatcherList
 import com.example.pegas.main.presentation.NavigationCommunication
+import com.example.pegas.response.data.IdForwardDoc
 import com.example.pegas.response.data.cache.CacheModule
 import com.example.pegas.response.data.cloud.CloudModule
 
-interface Core : CloudModule, CacheModule, ProvideNavigation{
+interface Core : CloudModule, CacheModule, ProvideNavigation, ProvideIdForwardDoc{
 
     fun provideDispatchers(): DispatcherList
+
 
     class Base(
         context: Context,
         private val provideInstances: ProvideInstances
     ) : Core {
+        private val idForwardDoc = IdForwardDoc.Base()
         private val navigationCommunication = NavigationCommunication.Base()
         private val dispatcherList by lazy {
             DispatcherList.Base()
@@ -27,10 +30,14 @@ interface Core : CloudModule, CacheModule, ProvideNavigation{
 
         override fun provideDispatchers() = dispatcherList
         override fun provideNavigation() = navigationCommunication
-        }
+        override fun provideIdForwardDoc() = idForwardDoc
+    }
     }
 
 
 interface ProvideNavigation {
     fun provideNavigation(): NavigationCommunication.Mutable
+}
+interface ProvideIdForwardDoc {
+    fun provideIdForwardDoc(): IdForwardDoc.Mutable
 }
