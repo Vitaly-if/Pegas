fde0b8aba38154b5c0c4f69c023658aa1c38c7b5
Adminius <vnakvasin@yandex.ru>
2022-12-01 12:23:28 +0700

    fix ResponseFragment.kt ResponseRepository.kt

--- app/src/main/java/com/example/pegas/response/domain/ResponseRepository.kt
+++ app/src/main/java/com/example/pegas/response/domain/ResponseRepository.kt
@@ -1,4 +1,7 @@
 package com.example.pegas.response.domain
 
+import com.example.pegas.response.data.ForwardDocData
+
 interface ResponseRepository {
+    suspend fun fetchForwardDoc(id : String): ForwardDocData
 }
